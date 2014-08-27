206c7120b898197f990394ce32883a44d0ad1e58
simonbcky <bicsek@gmail.com>
2014-08-27 13:42:32 +0000

    added html and app.use

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require("express");
-
+var express = require('express');
 var app = express();
 
-app.get('', function(res){res.send('clent/index.html');})
+app.get('/hello', function (req, res) {
+  res.send('Hello World');
+})
 
-console.log("runnin");
+app.use('/', express.static(__dirname + "/client"));
 
+//c9.io environment specific PORT
+app.listen(process.env.PORT, function() {console.log("listening")});
